8a9d129aaa256aecd8754e2af21caf0682b06b6c
Grace <cgracesharpe@gmail.com>
2022-05-08 15:03:43 -0400

    GD:implemented client mutations for login,signup,and workout

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,56 +1,11 @@
 import { gql } from '@apollo/client';
 
-
-
-export const QUERY_WORKOUT = gql`
-  query getCheckout($products: [ID]!) {
-    checkout(products: $products) {
-      session
-    }
-  }
-`;
-// export const QUERY_CALORIES = gql`
-//   {
-//     categories {
-//       _id
-//       name
-//     }
-//   }
-// `;
-
-export const QUERY_PROGRESS= gql`
-  {
-    products {
-      _id
-      name
-      description
-      price
-      quantity
-      category {
-        name
-      }
-    }
-  }
-`;
-
-
 export const QUERY_USER = gql`
-  {
-    user {
-      firstName
-      lastName
-      orders {
-        _id
-        purchaseDate
-        products {
-          _id
-          name
-          description
-          price
-          quantity
-          image
-        }
-      }
-    }
+query user($username: String!) {
+  user(username: $username) {
+    _id
+    username
+    email
+    workouts
   }
-`;
+}`
