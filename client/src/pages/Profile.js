94991632a2c85568241a7926104632dd2e36bb3c
Grace <cgracesharpe@gmail.com>
2022-05-10 17:14:29 -0400

    GD: Continued working on profile functionality, finished mutations and queries

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,20 +1,28 @@
+// react imports
 import React from "react";
 import { Redirect, useParams } from 'react-router-dom';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
-import { QUERY_USER, QUERY_USER_BRIEF } from '../utils/queries';
-import { useQuery } from '@apollo/client';
+// functionality variables/queries/mutations
+import { QUERY_USER } from '../utils/queries';
+import { useQuery, useMutation } from '@apollo/client';
+import { ADD_WORKOUT } from "../utils/mutations";
+import { SavedWorkouts } from "./CreateWorkouts";
+// in future {ADD_FRIEND, ADD_LOG, ADD_PROGRESS, ADD_PLAYLIST}
+
+
 import Auth from '../utils/auth';
 import './Styles/Pages.css';
 
 
-const Profile = (props) => {
+const Profile = () => {
     const { username: userParam } = useParams();
-    const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_USER_BRIEF, {
-        variables: { username: userParam },
+    const { addWorkout } = useMutation(ADD_WORKOUT);    
+    const { loading, data } = useQuery(userParam, (QUERY_USER), {
+        variables: { username: userParam }
     });
-    const user = data?.user;
+    const userData = data?.user;
 
     if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
         return <Redirect to="/profile" />;
@@ -24,19 +32,29 @@ const Profile = (props) => {
         return <div>Loading...</div>;
     }
 
-    // if (!user?.username) {
-    //     return (
-    //         <h4>
-    //             You need to be logged in to see this. Use the navigation links above to
-    //             sign up or log in!
-    //         </h4>
-    //     );
-    // }
+    if (!userData?.username) {
+        return (
+            <h4>
+                You need to be logged in to see this. Use the navigation links above to
+                sign up or log in!
+            </h4>
+        );
+    }
+
+    const handleClick = async () => {
+        // try {
+        //     await addWorkout({
+        //         variables: { id: user._id },
+        //     });
+        // } catch (e) {
+        //     console.error(e);
+        // }
+    };
 
     return (
         <div className="profile-wrapper">
             <div className="profile-contain">
-                <h1>Viewing {userParam ? `${user.username}'s` : 'your'} profile</h1>
+                <h1>Welcome Back {`${userData.user}`}!</h1>
                 <div className="profile-nav">
                     <Tabs>
                         <TabList>
@@ -48,10 +66,22 @@ const Profile = (props) => {
                         </TabList>
 
                         <TabPanel>
-                            <h2>Any content 1</h2>
+                            <h2>
+                                {/* list of saved workouts for user */}
+                                <SavedWorkouts
+                                    workouts={userData.workouts}
+                                    title={`${userData.username}'s workouts...`}
+                                />
+                                {/* button prompt for adding new empty workout -- will be redirected to createWorkout */}
+                                {userParam && (
+                                    <button className="btn ml-auto" onClick={handleClick}>
+                                        Add Workout
+                                    </button>
+                                )}
+                            </h2>
                         </TabPanel>
                         <TabPanel>
-                            <h2>Any content 2</h2>
+                            <h2>Coming Soon!</h2>
                         </TabPanel>
                         <TabPanel>
                             <h2>Coming Soon!</h2>
@@ -65,7 +95,7 @@ const Profile = (props) => {
                     </Tabs>
                 </div>
             </div>
-        </div>
+        </div >
     );
 
 
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,34 +11,28 @@ mutation login($email: String!, $password: String!) {
 }
 `;
 export const ADD_USER = gql`
-  mutation addUser(
-    $username: String!
-    $email: String!
-    $password: String!
-  ) {
+  mutation addUser($username: String!,$email: String!,$password: String!) {
     addUser(
       username: $username
       email: $email
       password: $password
-    ) {
+    ) 
+    {
       token
       user {
         _id
-      }
-    
+      }    
   }
 }
 `;
 
 export const ADD_WORKOUT = gql`
-mutation addWorkout($name: String!, $id: ID!) {
-  workoutname: String
-  
-  user{
-    _id
-    username
-  }
-}`
+  mutation addWorkout($workouts: String!) {
+    addWorkout(name: $workouts)  {
+      _id
+      name
+    }  
+  }`
 
 //remove exercises in it
 export const REMOVE_WORKOUT = gql `
@@ -56,6 +50,26 @@ mutation removeWorkout($name: String!, $id: ID!) {
   }
 }`
 
-
 //add exercise
+export const ADD_EXERCISE_TO_WORKOUT= gql `
+  mutation addExerciseToWorkout($id: ID!) {
+    workouts{
+      _id
+      name
+    }
+    exercises{
+      _id
+      name
+    }
+  }`
+
 //remove exercise 
+export const REMOVE_EXERCISE_TO_WORKOUT= gql `
+  mutation removeExerciseToWorkout ($id: ID!){
+    workouts{
+      _id
+    }
+    exercises{
+      _id
+    }
+  }`
