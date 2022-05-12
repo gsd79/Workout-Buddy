95a2be8257d311600fc27e416cb86c1e331f1e02
White Tiger <dayley.zimmermann@gmail.com>
2022-05-11 20:33:53 -0400

    added if production into server.js

--- server/server.js
+++ server/server.js
@@ -36,9 +36,9 @@ app.use(express.json());
 // // Serve up static assets
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
