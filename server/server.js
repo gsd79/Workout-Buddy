2529cc102df97081ec0fa2e7a5dc1aa50668cfc5
White Tiger <dayley.zimmermann@gmail.com>
2022-05-19 14:34:57 -0400

    removing signs of localhost and favicon

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const startServer = async () => {
   });
   await server.start();
   server.applyMiddleware({ app });
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
 startServer();
