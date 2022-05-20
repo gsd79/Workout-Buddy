97955afa62fe196dad886204a471ca90ed8090f0
White Tiger <dayley.zimmermann@gmail.com>
2022-05-19 20:36:25 -0400

    connection modifications

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout-bud', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/workout', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
