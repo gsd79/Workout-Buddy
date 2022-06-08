328b434b25e3d17610d4990b43e1ebee59a48b25
White Tiger <dayley.zimmermann@gmail.com>
2022-06-08 14:50:59 -0400

    latest version 6/8 from main merge

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
